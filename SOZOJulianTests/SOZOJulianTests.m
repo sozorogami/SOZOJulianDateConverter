668fa06d9b04cae7026eb4c33576e83a3bf12e14
modocache <modocache@gmail.com>
2014-02-13 22:53:14 +0900

    Add Travis & one failing test.

--- SOZOJulianTests/SOZOJulianTests.m
+++ SOZOJulianTests/SOZOJulianTests.m
@@ -10,12 +10,26 @@
 #import "SOZOJulianDate.h"
 #import "SOZOJulianTests.h"
 
+@interface SOZOJulianTests ()
+@property (nonatomic, strong) NSDateFormatter *dateFormatter;
+@end
+
 @implementation SOZOJulianTests
+
+- (void)setUp {
+    self.dateFormatter = [NSDateFormatter new];
+}
 
 - (void)testExample {
     SOZOGregorianDate *bc = [[SOZOGregorianDate alloc] initWithYear:-586 month:7 day:24];
     SOZOJulianDate *converted = [SOZOJulianDate dateFromFixed:[bc asFixed]];
     STAssertEquals([converted year], -587, @"Year is wrong.");
+}
+
+- (void)testConvertingGregorianToJulian {
+    STAssertEquals([SOZODateConverter gregorianDateFromJulianYear:1436 month:1 day:5],
+                   [self.dateFormatter dateFromString:@"February 3rd, 1436"],
+                   @"Expected Julian date of 1/5/1436 to be converted to Gregorian 2/3/1436.");
 }
 
 @end
