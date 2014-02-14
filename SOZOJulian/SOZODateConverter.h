905cb9ecf529aef2c9857529797f4fd2376df31d
Tyler Tape <tyler.tape@gmail.com>
2014-02-14 16:31:45 +0900

    Fixed broken tests and made passing.
    Added SOZODateConverter class.

---
+++ SOZOJulian/SOZODateConverter.h
@@ -0,0 +1,15 @@
+//
+//  SOZODateConverter.h
+//  SOZOJulian
+//
+//  Created by Tyler Tape on 2/14/14.
+//  Copyright (c) 2014 sozorogami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOZODateConverter : NSObject
+
++ (NSDate*)gregorianDateFromJulianYear:(int)year month:(int)month day:(int)day;
+
+@end
