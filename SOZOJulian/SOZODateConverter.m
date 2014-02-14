fd1b8e1d9f3f29256094985f3bdb5f482e74374c
Tyler Tape <tyler.tape@gmail.com>
2014-02-14 18:58:28 +0900

    Exception for bad Julian year zero.
    Added more tests.

--- SOZOJulian/SOZODateConverter.m
+++ SOZOJulian/SOZODateConverter.m
@@ -13,28 +13,42 @@
 
 @implementation SOZODateConverter
 
++ (NSException *)zeroYearException{
+    return [NSException
+            exceptionWithName:@"JulianZeroYearException"
+            reason:@"Julian Calendar does not have a year zero."
+            userInfo:nil];
+}
+
 + (NSDate*)gregorianDateFromJulianYear:(int)year month:(int)month day:(int)day{
+    
+    if (year == 0){
+        [[self zeroYearException] raise];
+    }
     
     SOZOJulianDate *julian = [[SOZOJulianDate alloc] initWithYear:year month:month day:day];
     SOZOGregorianDate *gregorian = [SOZOGregorianDate dateFromFixed:[julian asFixed]];
     
     NSDateComponents* comps = [[NSDateComponents alloc] init];
+    
+    if (year < 0) {
+        //Compensate for missing year 0.
+        [comps setYear:[gregorian year] + 1];
+    } else  {
+        [comps setYear:[gregorian year]];
+    }
+    
     [comps setDay:[gregorian day]];
     [comps setMonth:[gregorian month]];
-    [comps setYear:[gregorian year]];
     [comps setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
     [comps setHour:12];
     [comps setMinute:0];
     [comps setSecond:0];
     
-    NSLog(@"Gregorian: %@",gregorian);
-    
     NSCalendar *gregcal = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
     NSDate *converted = [gregcal dateFromComponents:comps];
     
-    NSLog(@"Converted: %@",converted);
-    NSDateFormatter *formatter = [NSDateFormatter new];
-    NSLog(@"Target: %@",[formatter dateFromString:@"February 3, 1436"]);
+    NSLog(@"CONVERTED: %@",converted);
     
     return converted;
     
--- SOZOJulianTests/SOZOJulianTests.m
+++ SOZOJulianTests/SOZOJulianTests.m
@@ -20,7 +20,7 @@
 - (void)setUp {
     self.dateFormatter = [[NSDateFormatter alloc] init];
     [self.dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
-    [self.dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    [self.dateFormatter setDateFormat:@"yyyy-MM-dd GG HH:mm:ss"];
 }
 
 - (void)testExample {
@@ -30,7 +30,18 @@
 }
 
 - (void)testConvertingJulianToGregorian {
-    STAssertTrue([[SOZODateConverter gregorianDateFromJulianYear:1436 month:1 day:25] isEqualToDate:[self.dateFormatter dateFromString:@"1436-02-03 12:00:00"]], @"Expected Julian date of 1/25/1436 to be converted to Gregorian 2/3/1436.");
+    STAssertTrue([[SOZODateConverter gregorianDateFromJulianYear:1436 month:1 day:25] isEqualToDate:[self.dateFormatter dateFromString:@"1436-02-03 AD 12:00:00"]], @"Expected Julian date of 1/25/1436 to be converted to Gregorian 2/3/1436.");
+    STAssertTrue([[SOZODateConverter gregorianDateFromJulianYear:1582 month:10 day:4] isEqualToDate:[self.dateFormatter dateFromString:@"1582-10-14 AD 12:00:00"]], @"Expected Julian date of 10/4/1582 to be converted to Gregorian 10/14/1582.");
+    STAssertTrue([[SOZODateConverter gregorianDateFromJulianYear:-587 month:7 day:30] isEqualToDate:[self.dateFormatter dateFromString:@"0586-7-24 BC 12:00:00"]], @"Expected Julian date of 7/30/587 BC to be converted to Gregorian 7/24/586 BC.");
+}
+
+- (void)testDatesNearEraChangeover {
+    STAssertTrue([[SOZODateConverter gregorianDateFromJulianYear:-1 month:1 day:3] isEqualToDate:[self.dateFormatter dateFromString:@"0001-01-01 AD 12:00:00"]], @"Julian date 1/3/1 BC represents the first day of the Gregorian calendar (1/1/1 AD)");
+    STAssertTrue([[SOZODateConverter gregorianDateFromJulianYear:-1 month:1 day:2] isEqualToDate:[self.dateFormatter dateFromString:@"0001-12-31 BC 12:00:00"]], @"Julian date 1/2/1 BC represents the first day before the Gregorian calendar (12/31/1 BC)");
+}
+
+- (void)testZeroYear {
+    STAssertThrows([SOZODateConverter gregorianDateFromJulianYear:0 month:1 day:1], @"Should not accept year zero.");
 }
 
 @end
